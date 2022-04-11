7ebb6f7534b1f8b878a05de9485ae470e64d3612
Matias <matiasonores@gmail.com>
2022-04-11 11:21:50 -0300

    primera subida

---
+++ Ejercicio 21/ArregloDigitos.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class ArregloDigitos
+{
+   private int x;
+
+   public int ObtenerCifras(int x)
+   {
+      this.x = x;
+      int contador = 0;
+
+      while (x % 10 != 0 || (x % 10 == 0 && x > 0))
+      {
+         x = x / 10;
+         contador++;
+      }
+      return contador;
+   }
+
+   public int[] ObtenerResto(int contador)
+   {
+      int resto;
+      int[] arreglo = new int[contador];
+
+      for (int i = 0; i < arreglo.Length; i++)
+      {
+         resto = x % 10;
+         this.x = x / 10;
+         arreglo[i] = resto;
+      }
+      return arreglo;
+   }
+
+   public int[] InvertirArreglo(int[] arreglo)
+   {
+      int[] array = new int[arreglo.Length];
+
+      for (int i = 0; i < arreglo.Length; i++)
+      {
+         array[i] = arreglo[arreglo.Length - 1-i];
+      }
+      return array;
+   }
+}
---
+++ Ejercicio 21/Ejercicio 21.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+   class Ejercicio_21
+   {
+      static void Main(string[] args)
+      {
+      ArregloDigitos miArregloDigitos = new ArregloDigitos();
+      Console.WriteLine("Ingrese un valor para obtener el número de cifras");
+      
+      int x = Convert.ToInt32(Console.ReadLine());
+      int contador = miArregloDigitos.ObtenerCifras(x);
+      
+      Console.WriteLine("El número tiene {0} cifra/s\n", contador);
+      
+      int[] arreglo = miArregloDigitos.ObtenerResto(contador);
+      int[] arregloInvertido= miArregloDigitos.InvertirArreglo(arreglo);
+
+      Console.WriteLine("Arreglo generado con los valores del número ingresado:");
+      for (int i=0;i<arregloInvertido.Length; i++)
+      {
+         Console.WriteLine("{0}",arregloInvertido[i]);
+      }
+      }
+   }
+
